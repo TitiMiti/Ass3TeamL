c7b0babb32928cc04c4e3a0bc670eb700128c23e
Zhuldyz20 <zhuldyz.ilyassova2001@gmail.com>
2021-02-07 17:47:56 +0600

    Added new Zhuldyz activity

---
+++ app/src/main/java/com/teamH/assignment3/ZhuldyzActivity.java
@@ -0,0 +1,14 @@
+package com.teamH.assignment3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ZhuldyzActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_zhuldyz);
+    }
+}
